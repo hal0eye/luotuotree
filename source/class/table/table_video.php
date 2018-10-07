b1169b12e7d1697502f16e03f86b28936d50bcd7
zhangxingjia <xjzhang@hillinsight.com>
2018-10-07 13:54:06 +0800

    fix

--- source/class/table/table_video.php
+++ source/class/table/table_video.php
@@ -5,6 +5,29 @@ if(!defined('IN_DISCUZ')) {
 }
 
 class table_video extends discuz_table{
+
+    private static $course_type_map = [
+        '1' => '社群招生',
+        '2' => '教学教研',
+        '3' => '课程销售'
+    ];
+
+    private static $charge_type_map = [
+        '1' => '公开课',
+        '2' => '会员课程',
+        '3' => '收费课程'
+    ];
+
+    private static $is_remind_map = [
+        '1' => '是',
+        '0' => '否',
+    ];
+
+    private static $is_out_of_stock_map = [
+        '1' => '是',
+        '0' => '否',
+    ];
+
     public function __construct() {
         $this->_table = 'video';
         $this->_pk    = 'id';
@@ -13,6 +36,10 @@ class table_video extends discuz_table{
 
     public function get_video_list($page){
         $result = DB::fetch_all("SELECT * FROM ".DB::table('video')." WHERE is_out_of_stock = 0" . DB::limit($page*10-10,10));
+        foreach ($result as &$value) {
+            $value['course_type'] = isset(self::$course_type_map[$value['course_type']]) ? self::$course_type_map[$value['course_type']] : '';
+            $value['charge_type'] = isset(self::$charge_type_map[$value['charge_type']]) ? self::$charge_type_map[$value['charge_type']] : '';
+        }
         return $result;
     }
 
@@ -23,6 +50,10 @@ class table_video extends discuz_table{
 
     public function get_video_info($id){
         $result = DB::fetch_all("SELECT * FROM ".DB::table('video')." WHERE is_out_of_stock = 0 AND id = {$id}");
+        foreach ($result as &$value) {
+            $value['course_type'] = isset(self::$course_type_map[$value['course_type']]) ? self::$course_type_map[$value['course_type']] : '';
+            $value['charge_type'] = isset(self::$charge_type_map[$value['charge_type']]) ? self::$charge_type_map[$value['charge_type']] : '';
+        }
         return $result;
     }
 
